7ae85a6e21d9456a6e6ce4ad566d58e85fb3aea0
gBogdan95 <bogdan.ghinga@gmail.com>
2024-03-28 17:31:00 +0200

    added notification functionality for friend requests

---
+++ socialmedia-frontend/src/components/FriendRequestNotification.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { Box, Button, Typography, Avatar } from "@mui/material";
+import defaultAvatarImg from "../assets/defaultAvatar.png";
+
+interface FriendRequestNotificationProps {
+  requesterId: string;
+  username: string;
+  avatarUrl?: string | null;
+  onAccept: () => void;
+  onDecline: () => void;
+}
+
+const FriendRequestNotification: React.FC<FriendRequestNotificationProps> = ({
+  username,
+  avatarUrl,
+  onAccept,
+  onDecline,
+}) => {
+  return (
+    <Box
+      sx={{
+        p: 2,
+        border: "1px solid",
+        borderRadius: "4px",
+        width: "92.05%",
+        backgroundColor: "#F5F7F8",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          mb: 2,
+        }}
+      >
+        <Avatar
+          src={avatarUrl || defaultAvatarImg}
+          alt={username}
+          sx={{ mr: 2 }}
+        />
+        <Typography variant="subtitle1" sx={{ cursor: "default" }}>
+          <span style={{ fontWeight: "bold" }}>{username}</span>
+          {" sent you a friend request!"}
+        </Typography>
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+        <Button
+          variant="contained"
+          color="success"
+          onClick={onAccept}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            mr: 2,
+            width: "100px",
+            color: "white",
+            backgroundColor: "#40A2D8",
+            ":hover": {
+              backgroundColor: "#1450A3",
+            },
+          }}
+        >
+          Accept
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={onDecline}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            mr: 1,
+            width: "100px",
+            color: "white",
+            backgroundColor: "#D24545",
+            ":hover": {
+              backgroundColor: "red",
+            },
+          }}
+        >
+          Decline
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default FriendRequestNotification;
--- socialmedia-frontend/src/pages/Notifications.tsx
+++ socialmedia-frontend/src/pages/Notifications.tsx
@@ -1,19 +1,81 @@
-import React from "react";
-import Box from "@mui/material/Box";
+import React, { useState, useEffect } from "react";
+import { Box, CircularProgress, Typography } from "@mui/material";
+import FriendRequestNotification from "../components/FriendRequestNotification";
+import { friendshipService } from "../services/friendshipService";
 
-const Notifications = () => {
+interface FriendRequest {
+  id: string;
+  requester: {
+    id: string;
+    username: string;
+    avatarUrl?: string | null;
+  };
+  status: string;
+}
+
+const NotificationsPage = () => {
+  const [friendRequests, setFriendRequests] = useState<FriendRequest[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchFriendRequests = async () => {
+      setLoading(true);
+      try {
+        const data = await friendshipService.listPendingFriendRequests();
+        setFriendRequests(data);
+      } catch (error) {
+        console.error("Failed to load friend requests:", error);
+        setError("Failed to load friend requests.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchFriendRequests();
+  }, []);
+
+  const handleAccept = async (friendRequestId: string) => {};
+
+  const handleDecline = async (friendRequestId: string) => {};
+
+  if (loading) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return <Typography color="error">{error}</Typography>;
+  }
+
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      height="100%"
-      width="100%"
-      sx={{ backgroundColor: "grey" }}
-    >
-      <h3>NOTIFICATIONS PAGE</h3>
+    <Box>
+      {friendRequests.length > 0 ? (
+        friendRequests.map((request) => (
+          <FriendRequestNotification
+            key={request.id}
+            requesterId={request.requester.id}
+            username={request.requester.username}
+            avatarUrl={request.requester.avatarUrl}
+            onAccept={() => handleAccept(request.id)}
+            onDecline={() => handleDecline(request.id)}
+          />
+        ))
+      ) : (
+        <Typography>No new notifications</Typography>
+      )}
     </Box>
   );
 };
 
-export default Notifications;
+export default NotificationsPage;
