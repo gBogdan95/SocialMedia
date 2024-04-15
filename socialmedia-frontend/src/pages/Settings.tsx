d0c90f5055c50c82408bb92f69ada8310900a4a7
gBogdan95 <bogdan.ghinga@gmail.com>
2024-04-15 14:32:50 +0300

    started implementing Settings page

--- socialmedia-frontend/src/pages/Settings.tsx
+++ socialmedia-frontend/src/pages/Settings.tsx
@@ -1,16 +1,76 @@
 import React from "react";
-import Box from "@mui/material/Box";
+import { Box, Button, Typography } from "@mui/material";
 
-const Settings = () => {
+const Settings: React.FC = () => {
   return (
     <Box
       display="flex"
+      flexDirection="column"
       alignItems="center"
       justifyContent="center"
-      height="100%"
-      width="100%"
+      height="100vh"
     >
-      <h1>SETTINGS PAGE</h1>
+      <Box width="100%" maxWidth={700}>
+        <Typography
+          variant="h6"
+          sx={{
+            fontSize: 25,
+            bgcolor: "#1450A3",
+            color: "common.white",
+            cursor: "default",
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 1)",
+            py: 2.5,
+            textAlign: "center",
+            borderTopLeftRadius: 4,
+            borderTopRightRadius: 4,
+          }}
+        >
+          ACCOUNT
+        </Typography>
+        <Box sx={{ border: 1, borderColor: "black" }}>
+          {["Change Username", "Change Password", "Change Email"].map(
+            (text, index) => (
+              <Button
+                key={text}
+                variant="text"
+                fullWidth
+                sx={{
+                  fontSize: 20,
+                  color: "text.primary",
+                  py: 3,
+                  borderColor: "black",
+                  "&:not(:last-child)": {
+                    borderBottom: 1,
+                  },
+                  "&:hover": {
+                    bgcolor: "primary.secondary",
+                  },
+                  borderRadius: 0,
+                  boxShadow: "none",
+                }}
+              >
+                {text}
+              </Button>
+            )
+          )}
+        </Box>
+        <Typography
+          variant="h6"
+          sx={{
+            fontSize: 25,
+            bgcolor: "#1450A3",
+            color: "common.white",
+            cursor: "default",
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 1)",
+            py: 2.5,
+            textAlign: "center",
+            borderTopLeftRadius: 0,
+            borderTopRightRadius: 0,
+          }}
+        >
+          PRIVACY
+        </Typography>
+      </Box>
     </Box>
   );
 };
