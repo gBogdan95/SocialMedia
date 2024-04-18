02a66acc259bc2c15c6c4ec9a6950aad1a66b68c
gBogdan95 <bogdan.ghinga@gmail.com>
2024-04-18 18:20:39 +0300

    fixed design of avatar in home page

--- socialmedia-frontend/src/pages/Home.tsx
+++ socialmedia-frontend/src/pages/Home.tsx
@@ -108,7 +108,6 @@ const Home = () => {
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
             borderRadius: "50%",
           }}
         >
